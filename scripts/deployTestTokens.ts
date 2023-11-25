274d47f3ca2bf40e96d070b88807ffe965bc054b
Jack <herrymarry@proton.me>
2023-11-25 17:00:45 +0800

    minor update

--- scripts/deployTestTokens.ts
+++ scripts/deployTestTokens.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat";
+import hre from "hardhat";
 
 async function main() {
   console.log("Deploying test tokens...");
@@ -14,11 +15,23 @@ async function main() {
   let balance = await weth.balanceOf(owner.address);
   console.log(`WETH Balance of Owner: ${balance}`);
 
+  await hre.run("verify:verify", {
+    address: weth.target,
+    constructorArguments: ["Wrapped Ethereum", "WETH"],
+    contract: "contracts/TestToken.sol:TestToken"
+  });
+
   const usdc = await ethers.deployContract("TestToken", ["USD Coin", "USDC"]);
 
   console.log(`USDC deployed. Address: ${usdc.target}`);
   balance = await usdc.balanceOf(owner.address);
   console.log(`USDC Balance of Owner: ${balance}`);
+
+  await hre.run("verify:verify", {
+    address: usdc.target,
+    constructorArguments: ["USD Coin", "USDC"],
+    contract: "contracts/TestToken.sol:TestToken"
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
