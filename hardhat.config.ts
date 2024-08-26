7a57d55c8b8615e7f21180cf9ee22bd857a7904d
narutomanchester <narutomanchester@gmail.com>
2024-08-26 13:03:01 +0700

    add timeout

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,6 +20,7 @@ const mantleSepolia: NetworkUserConfig = {
   url: "https://rpc.sepolia.mantle.xyz",
   chainId: 5003,
   accounts: [process.env.KEY_TESTNET!],
+  gasPrice: 30000000,
 };
 const mantleMainnet: NetworkUserConfig = {
   url: "https://rpc.mantle.xyz",
@@ -124,6 +125,9 @@ const config = {
         },
       }
     ],
+  },
+  mocha: {
+    timeout: 300000000,
   },
   paths: {
     sources: "./contracts/",
